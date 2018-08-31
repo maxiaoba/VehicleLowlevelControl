de3f80fe86e9451d668e4c80fe7ddc57f07e9195
maxiaoba <maxiaoba@umich.edu>
2018-08-31 14:12:20 -0700

    add Ad05RTheta1RSteer05RSmooth1Abs0FR200

---
+++ Ad05RTheta1RSteer05RSmooth1Abs0FR200/AccCurvatureStateDriver.jl
@@ -0,0 +1,51 @@
+
+export AccCurvatureStateDriver
+
+mutable struct AccCurvatureStateDriver <: DriverModel{AccelSteeringDirection}
+    dt::Float64
+    acc::Float64
+    u::Float64 #commadn steer
+    c::Array{Float64}
+    direction::Int
+    Ad::Array{Float64}
+    Bd::Array{Float64}
+    Cd::Array{Float64}
+    steer::Float64
+    k::Float64 #actual curvature
+    t::Int
+    acc_old::Float64
+    steer_old::Float64
+end
+
+function AccCurvatureStateDriver(dt::Float64, a::Float64, u::Float64,t::Int;
+        c::Array{Float64}=zeros(2,1), direction::Int=1,
+        A::Array{Float64} = ACONST,
+        B::Array{Float64} = BCONST,
+        C::Array{Float64} = CCONST,
+        steer::Float64 = 0.0,
+        k::Float64 = 0.0,
+        acc_old::Float64 = 0.0,
+        steer_old::Float64 = 0.0,
+        )
+    M=expm([A B; 0 0 0]*dt)
+    Ad=M[1:2,1:2]
+    Bd=M[1:2,3]
+    Cd = C
+    return AccCurvatureStateDriver(dt,a,u,c,direction,Ad,Bd,Cd,steer,k,t,acc_old,steer_old)
+end
+
+AutomotiveDrivingModels.get_name(model::AccCurvatureStateDriver) = "AccCurvatureStateDriver"
+
+function AutomotiveDrivingModels.observe!(model::AccCurvatureStateDriver, scene::Scene, roadway::Roadway, egoid::Int)
+    model
+end
+
+function Base.rand(model::AccCurvatureStateDriver)
+    L = AXLE_DISTANCE
+    model.c = model.Ad*model.c + model.Bd*tan.(model.u)/L
+    model.k = (model.Cd*model.c)[1]
+    model.steer = atan(L*model.k)
+    return AccelSteeringDirection(model.acc,model.steer,model.direction)
+end
+Distributions.pdf(model::AccCurvatureStateDriver, a::Float64) = a == model.acc ? Inf : 0.0
+Distributions.logpdf(model::AccCurvatureStateDriver, a::Float64) = a == model.acc ? Inf : -Inf
---
+++ Ad05RTheta1RSteer05RSmooth1Abs0FR200/rewards.jl
@@ -0,0 +1,66 @@
+function log_speed(speed)
+    x = clamp(speed,MINIMUM_SPEED,DESIRE_SPEED)
+    x = (speed/MAXIMUM_SPEED) * 100.0 + 0.99
+    x = max(0,x)
+    base = MAXIMUM_SPEED/2.0
+    reward = max(0.0, log(base, x) / 2.0)
+    reward -= 0.5
+    reward *= 2
+    return reward
+end
+
+function reward_fn(action::Egoaction,fail::Bool,
+        scene::Union{Scene,Frame{Entity{VehicleState, BicycleModel, Int}}},models::Dict{Int, DriverModel},roadway::Roadway)
+    if fail
+        return -200.0
+    end
+
+    acc = clamp(action.acc,-1.0,1.0)
+    steer = clamp(action.steer,-1.0,1.0)
+
+    smoothReward = (acc-models[1].acc_old)^2+(steer-models[1].steer_old)^2
+    models[1].acc_old = action.acc
+    models[1].steer_old = action.steer
+
+    acc *= ACCELERATION_MAX
+    steer *= STEER_MAX
+    
+    speedReward = log_speed(scene[1].state.v)
+    Px = scene[1].state.posG.x
+    Py = scene[1].state.posG.y
+    V = scene[1].state.v
+    θ = mod2pi(scene[1].state.posG.θ)
+    goal = LANE_WIDTH
+    t = models[1].t
+    
+    goalReward = 0.0
+    if abs(Py-goal) < 0.05
+        goalReward = 3.0
+    elseif abs(Py-goal) < LANE_WIDTH
+        goalReward = 1.0-abs(Py-goal)/LANE_WIDTH
+    else
+        goalReward = 1.0-abs(Py-goal)/LANE_WIDTH
+    end
+    
+    accReward = -abs(acc)/ACCELERATION_MAX
+    steerReward = -abs(steer)/STEER_MAX
+    
+    thetaReward = 0.0
+    if θ>pi
+        θ = θ-2*pi
+    end
+    thetaReward = 1.0-abs.(θ)/(pi/4)
+    if abs(θ) > pi/4
+        thetaReward = -1.0
+    # else
+    #     if (goal > Py) && (θ < -0.1)
+    #         thetaReward = -1.0
+    #     elseif (goal < Py) && (θ > 0.1)
+    #         thetaReward = -1.0
+    #     else
+    #         thetaReward = 0.5-abs(θ)/(pi/4)
+    #     end
+    end
+    
+    return 0.5*speedReward+goalReward+0.1*accReward+0.5*steerReward+1.0*thetaReward+1.0*smoothReward
+end
